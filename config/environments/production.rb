dd0ef9b5787db08d8ff8f4ac1c0c4dfe637dc231
byeonghan.baek <byeonghan.baek@gmail.com>
2013-10-01 23:32:38 +0900

    Reupdate S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,15 +70,15 @@ Omrails::Application.configure do
   
   # Configuring Amazon S3 for Paperclip file uploads
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-    :path => ":class/:id/:basename_:style.:extension",
-    :url => ":s3_sg_url"
-  }
+          :storage =>; :s3,
+          :s3_credentials =>; {
+            :bucket =>; ENV['AWS_BUCKET'],
+            :access_key_id =>; ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key =>; ENV['AWS_SECRET_ACCESS_KEY']
+          },
+          :path =>; ":class/:id/:basename_:style.:extension",
+          :url =>; ":s3_sg_url"
+      }
 
 
 end
